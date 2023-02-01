115556221b477299f89a34c373cdc8776f6f5c89
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-01 19:48:08 -0300

    Js
    Js

--- ProjetosJS/write.js
+++ ProjetosJS/write.js
@@ -1,4 +1,4 @@
-/*document.write("<h1>Olá, mundo</h1>");
+document.write("<h1>Olá, mundo</h1>");
 document.write("<p>Agora ja sei como escrever na tela com JavaScript</p>");
 document.write("<p>Tem que usar o document.write()</p>");
 document.write("<h2>Proximos passos</h2>");
@@ -6,5 +6,5 @@ document.write("<p>Da pra escrever na tela usando variaveis, exemplo abaixo:</p>
 var nome = "Ranyeri"
 var sobreNome = "Cunha"
 document.write(nome + " " + sobreNome)
-document.getElementById(teste) = document.write(nome)*/
+document.write("<img src='https://www.google.com/google.jpg'</img>")
 
