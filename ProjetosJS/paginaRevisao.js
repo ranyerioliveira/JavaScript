175a92b453816fdbbfa29e7bfc3a5be6e066c85f
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-03 18:56:42 -0300

    Revisão 03/02
    03/02

--- ProjetosJS/paginaRevisao.js
+++ ProjetosJS/paginaRevisao.js
@@ -12,7 +12,7 @@ document.write("<p>O console.log serve para imprimir no console e ajudando a tes
 document.write("<p>console.log=(var1+var2)</p>");
 document.write("<h2>Refreash e Redirect</h2>")
 document.write("<p>Para adicionar um comando indicando para outra pagina para o botão se colcado o window.location.href = 'url'</p>")*/
-function ola(){
+/*function ola(){
     var texto = document.getElementById('vindo');
     var nome = prompt('Qual é seu nome?');
     texto.innerHTML = 'Meu nome é'+ ' ' + nome + '.';
@@ -29,4 +29,14 @@ function sobre(nome2){
     var texto = document.getElementById('sobre');
     var nome2 = prompt('Qual é seu nome?');
     texto.innerHTML = nome2 + ' ' + nome;
+}*/
+var n1 = prompt('Digite o numero 1');
+var n2 = prompt('Digite o numero 2');
+var media = (parseInt(n1)+parseInt(n2))/2;
+var score = document.getElementById('nota')
+
+if(media => 7){
+    score.innerHTML= 'Você passou';
+}else{
+    score.innerHTML = 'Você reprovou';
 }
