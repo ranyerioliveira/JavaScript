695592d6f592e3e30ac4c8099c282b1c0b707e82
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-03 14:57:01 -0300

    revisão
    revisão

---
+++ paginaRevisao.js
@@ -0,0 +1,14 @@
+document.write("<h1 style= color:red>Escrevendo com JS</h1>");
+document.write("<h2>Utilizando variaveis</h2>");
+var dado1 = 2
+var dado2 = 3
+/*var num1 = 20
+var num2 = 10
+console.log(num1 + num2)*/
+document.write("<p>-Calculando as variaveis e escrevendo no HTML com document.write<br> -var dado1=2, var dado2=3, 2+3=</p>");
+document.write("Resultado:" + " " + (dado1 + dado2))
+document.write("<h2>Utilizando o console.log</h2>")
+document.write("<p>O console.log serve para imprimir no console e ajudando a testar se o codigo esta correndo</p>");
+document.write("<p>console.log=(var1+var2)</p>");
+document.write("<h2>Refreash e Redirect</h2>")
+document.write("<p>Para adicionar um comando indicando para outra pagina para o botão se colcado o window.location.href = 'url'</p>")
