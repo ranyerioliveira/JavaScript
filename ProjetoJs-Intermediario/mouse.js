8b69584d5559185643b69bb0a3000e752488d029
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-07 21:58:32 -0300

    Atualização
    Atualização com eventos de mouse

---
+++ ProjetoJs-Intermediario/mouse.js
@@ -0,0 +1,13 @@
+function apertouomouse(){
+    console.log("Apertei o mouse");
+}
+
+function soltarmouse(){
+    console.log("Soltei o Mouse");
+}
+function passeiomouse(){
+    console.log("Passei o mouse sobre o botão")
+}
+function tireiomouse(){
+    console.log("Sai do botão")
+}
