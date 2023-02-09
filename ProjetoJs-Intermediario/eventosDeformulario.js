bd141f21703aeb5782137ecdb88ccb3fe7efe0f6
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-09 10:00:19 -0300

    Evenos de formulario
    Atualização pagina eventos de formularios

---
+++ ProjetoJs-Intermediario/eventosDeformulario.js
@@ -0,0 +1,12 @@
+function focado(){
+    console.log('Elemento focado');
+}
+function sairDoFoco(){
+    console.log('Sai do foco do elemento');
+}
+function selecionarCidade(cidade){
+    console.log('Você escolheu a cidade: ' + cidade.value)
+}
+function enviar(){
+    console.log('Enviar requisição')
+}
