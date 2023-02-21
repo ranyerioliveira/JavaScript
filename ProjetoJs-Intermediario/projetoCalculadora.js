a5d3c95a661286ae9d6b0e132911c8514cbb4e10
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-21 17:13:19 -0300

    Atualuzação
    Calculadora atualização CSS.
    Colocar a pagina calculadora no principal

--- ProjetoJs-Intermediario/projetoCalculadora.js
+++ ProjetoJs-Intermediario/projetoCalculadora.js
@@ -11,13 +11,36 @@ var imc;
 var resultado;
 
 function calcular(){
+
     peso = document.getElementById('peso').value;
     altura = document.getElementById('altura').value;
     imc = peso/(altura*altura);
 
     if(imc < 17){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br> Seu resultado foi:' + imc;
+        resultado.innerHTML = '<br> Seu resultado foi:' + imc.toFixed(2) + '<h3><br>Cuidado, você ta muito abaixo do peso</h3><br>';
+        document.getElementById('peso').value = '';
+        document.getElementById('altura').value = '';
+        return false;
+    }else if(imc > 17 && imc < 18.49){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br> Seu resultado foi:' + imc.toFixed(2) + '<h3><br>Cuidado, você ta abaixo do peso</h3><br>';
+        document.getElementById('peso').value = '';
+        document.getElementById('altura').value = '';
+        return false;
+    }else if(imc > 18.5 && imc < 24.49){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br> Seu resultado foi:' + imc.toFixed(2) + '<h3><br>Otimo, você ta no peso certo</h3><br>';
+        document.getElementById('peso').value = '';
+        document.getElementById('altura').value = '';
+        return false;
+    }else if(imc > 25 && imc < 29.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br> Seu resultado foi:' + imc.toFixed(2) + '<h3><br>Cuidado, você ta acima do peso</h3><br>';
+        document.getElementById('peso').value = '';
+        document.getElementById('altura').value = '';
+        return false;
+    }else{
         return false;
     }
 }
