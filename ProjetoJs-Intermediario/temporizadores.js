456f2c80f119903ee3ab027c7ae4289ae025d6ed
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-21 21:46:33 -0300

    Finalizando temporizador
    Teporizador

---
+++ ProjetoJs-Intermediario/temporizadores.js
@@ -0,0 +1,10 @@
+
+function acao(){
+   document.write("<ol><h2> onload</h2><p>É a função que mostra quando a página foi carregada codigo no JS</p><h2>*onunload</h2><p>É uma função pouco utilizada devido é a função que mostra a saída da pagina</p></ol>");
+}
+
+//executar tempo em tempo
+//setInterval(acao, 3000);
+//Executar só uma vez e parar
+//setTimeout(acao, 3000)
+//pode criar uma varia do setIterval e colocar a funação clearInterval, ai pode definir onde vai parar o loop
