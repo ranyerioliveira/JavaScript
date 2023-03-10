edc6234256579f1730457ef2c56bcf46ee27246f
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-03-09 22:08:30 -0300

    Atualização
    Atualização

--- ECMAScript6+/varElet.js
+++ ECMAScript6+/varElet.js
@@ -1,5 +1,11 @@
-var nome = "Ranyeri";
-if(nome == "Ranyeri"){
-    var idade = 34;
-    console.log("Ele tem"+" "+idade);
-}
+//var nome = "Ranyeri";
+//if(nome == "Ranyeri"){
+// var idade = 34;
+// console.log("Ele tem"+" "+idade);
+//}
+const pessoa = {
+    nome: "Ranyeri", sobrenome: "Cunha", idade: 34, cargo:"TI"
+    
+};
+let {nome} = pessoa;
+console.log(nome);
