cd73ceb25012578d43e00f78de06d2f7f2fddc42
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-03-20 20:03:33 -0300

    Opreaçoes dentro de uma array
    Opreçãoes

---
+++ ECMAScript6+/operacoesArray.js
@@ -0,0 +1,15 @@
+const lista = [1,2,3,4,5,6];
+const novaLista = lista.map(function(item){
+    return item *5;
+});
+console.log(novaLista);
+
+const soma = lista.reduce(function(total, proximo){
+    return total + proximo;
+})
+console.log(soma);
+
+const find = lista.find(function(item){
+    return item ===4;
+}) 
+console.log(find);
