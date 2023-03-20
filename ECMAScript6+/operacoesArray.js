fe3ea625e6a93f48558c8647f12a887e01da8d42
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-03-20 20:34:06 -0300

    reduce
    reduce

--- ECMAScript6+/operacoesArray.js
+++ ECMAScript6+/operacoesArray.js
@@ -1,15 +1,22 @@
-const lista = [1,2,3,4,5,6];
-const novaLista = lista.map(function(item){
-    return item *5;
-});
-console.log(novaLista);
+//const lista = [1,2,3,4,5,6];
+//const novaLista = lista.map(function(item){
+//    return item *5;
+//});
+//console.log(novaLista);
 
-const soma = lista.reduce(function(total, proximo){
-    return total + proximo;
-})
-console.log(soma);
+//const soma = lista.reduce(function(total, proximo){
+    //return total + proximo;
+//})
+//console.log(soma);
 
-const find = lista.find(function(item){
-    return item ===4;
-}) 
-console.log(find);
+//const find = lista.find(function(item){
+    //return item ===4;
+//}) 
+//console.log(find);
+function adicionar(...numeros){
+    let total = numeros.reduce((total, proximo) => {
+        return total + proximo;
+    })
+    console.log(total);
+}
+adicionar(1,2,3,4,5,6);
