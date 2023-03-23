ac0e8e4b94fb22be7daf457c382eaa6bf315ba24
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-03-23 19:51:32 -0300

    novos metodos JSecma6
    metodos

---
+++ ECMAScript6+/novasFuncoes.js
@@ -0,0 +1,9 @@
+//includes
+let nome = ['Ranyeri', 'Pedro', 'Chico']
+console.log(nome.includes('Rany'));
+
+if(nome.includes('Ranyeri')){
+    console.log('Ranyeri está na lista');}
+else{
+    console.log('Ranyeri não ta na lista');
+}
