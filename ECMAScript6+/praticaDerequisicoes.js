b270f25cb1693bfe7fab9e1ab10ca0e9867cf55f
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-03-30 21:33:52 -0300

    finalizando curso
    Finalizando

--- ECMAScript6+/praticaDerequisicoes.js
+++ ECMAScript6+/praticaDerequisicoes.js
@@ -4,7 +4,7 @@ var listElement = document.querySelector('#repos ul');
 
 var repositorios = [];
 
-function buscaRepos(){
+buscaRepos = async() => {
     const input = document.querySelector('#input');
 
     if(input.value === ''){
@@ -12,7 +12,14 @@ function buscaRepos(){
         return;
     }
 
-    const response = fetch(`https://api.github.com/repos/${input.value}`)
+    const hasRepo = repositorios.find(repo => repo.full_name === input.value);
+
+    if(hasRepo){
+        alert('Este repositório já existe')
+        return;
+    }
+
+    const response = await fetch(`https://api.github.com/repos/${input.value}`)
     .then(res => res.json())
     .then(json=>{console.log(json);
     repositorios.push(json);
@@ -35,11 +42,24 @@ function renderRepos(){
     avatarElement.src = repo.owner.avatar_url;
     avatarElement.setAttribute("style", "widht:24px; height:24px; border-radio:12px;");
 
+    let linkElement = document.createElement('button');
+    linkElement.textContent = 'Excluir';
+
+    let posicao = repositorios.indexOf(repo);
+
+    linkElement.setAttribute('onclick', 'deletarRepo('+posicao+')');
+
     let name = document.createTextNode(repo.full_name);
 
     repoElement.appendChild(avatarElement);
     repoElement.appendChild(name);
+    repoElement.appendChild(linkElement);
     listElement.appendChild(repoElement);
 
     }
+}
+
+function deletarRepo(posicao){
+    repositorios.splice(posicao, 1);
+    renderRepos();
 }
